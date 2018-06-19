12a6a787010eb224f9ec6d86eed732c82722bb08
coyalo03 <coyalo86@gmail.com>
2018-06-19 21:31:52 +0000

    automatically backed up by learn

---
+++ while.rb
@@ -0,0 +1,26 @@
+def using_while 
+counter = 0
+  
+ while counter < 10 
+    puts "Wingardium Leviosa"
+  
+   counter = counter + 1 
+     puts "#{counter} levitation force!!"
+   end 
+  
+ end 
+
+using_while
+
+def using_until
+counter = 20
+  
+  until counter == 0 do 
+    puts "Wingardium Leviosa"
+    
+counter -= 1 
+
+end 
+
+end 
+cupid_shuffle 
